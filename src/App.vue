562ef13be9e2006f69d958b157c0cd0336fe66d8
traveller <travellerinblog@gmail.com>
2017-08-22 11:53:52 +0900

    폰트수정

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,98 @@
 <template lang="pug">
   #app(v-cloak)
     .wrapper
-      a(href="#content" class="a11y-hidden ") 본문 바로가기 링크
+      a.a11y-hidden(href="#content") 본문 바로가기 링크
       router-view
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {}
+  export default {
+    name: 'app',
+    data () {
+      return {}
+    }
   }
-}
 </script>
 
 <style lang="scss">
-@import './sass/App';
-@font-face {
-  font-family: 'Nanum Square';
-  src: url('./assets/font/NanumSquareR.eot');
-  src: url('./assets/font/NanumSquareR.eot?#iefix') format('embedded-opentype'),
-       url('./assets/font/NanumSquareR.woff') format('woff'),
-       url('./assets/font/NanumSquareR.ttf')  format('truetype'),
-       url('./assets/font/NanumSquareOTFR.svg#9e23ceb0c858ca37642ba540402577eb') format('svg');
-  font-style: normal;
-  font-weight: 400;
-}
-@font-face {
-  font-family: 'Nanum Square';
-  src: url('./assets/font/NanumSquareB.eot');
-  src: url('./assets/font/NanumSquareB.eot?#iefix') format('embedded-opentype'),
-       url('./assets/font/NanumSquareB.woff') format('woff'),
-       url('./assets/font/NanumSquareB.ttf')  format('truetype'),
-       url('./assets/font/NanumSquareOTFB.svg#a41a1fda1fdf3dafd3394867a156b1cf') format('svg');
-  font-style: normal;
-  font-weight: 700;
-}
-@font-face {
-    font-family: 'Glyphter';
-    src: url('./assets/font/Glyphter.eot');
-    src: url('./assets/font/Glyphter.eot?#iefix') format('embedded-opentype'),
-         url('./assets/font/Glyphter.woff') format('woff'),
-         url('./assets/font/Glyphter.ttf') format('truetype'),
-         url('./assets/font/Glyphter.svg#Glyphter') format('svg');
-    font-weight: normal;
+  @import './sass/App';
+  @font-face {
+    font-family: 'Nanum Square';
+    src: url('./assets/font/NanumSquareR.eot');
+    src: url('./assets/font/NanumSquareR.eot?#iefix') format('embedded-opentype'), url('./assets/font/NanumSquareR.woff') format('woff'), url('./assets/font/NanumSquareR.ttf') format('truetype'), url('./assets/font/NanumSquareOTFR.svg#9e23ceb0c858ca37642ba540402577eb') format('svg');
     font-style: normal;
-}
-[class*='icon-']:before{
-	display: inline-block;
-   font-family: 'Glyphter';
-   font-style: normal;
-   font-weight: normal;
-   line-height: 1;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale
-}
-.icon-search24:before{content:'\0041';}
-.icon-back:before{content:'\0042';}
-.icon-delete:before{content:'\0043';}
-.icon-down:before{content:'\0044';}
-.icon-next:before{content:'\0045';}
-.icon-up:before{content:'\0046';}
-#app {
-  font-family: 'Nanum Square','Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #000;
-}
-[v-cloak] { 
-  display: none 
-}
+    font-weight: 400;
+  }
+  
+  @font-face {
+    font-family: 'Nanum Square';
+    src: url('./assets/font/NanumSquareB.eot');
+    src: url('./assets/font/NanumSquareB.eot?#iefix') format('embedded-opentype'), url('./assets/font/NanumSquareB.woff') format('woff'), url('./assets/font/NanumSquareB.ttf') format('truetype'), url('./assets/font/NanumSquareOTFB.svg#a41a1fda1fdf3dafd3394867a156b1cf') format('svg');
+    font-style: normal;
+    font-weight: 700;
+  }
+  
+  @font-face {
+    font-family: "travelericon";
+    src: url("./assets/font/travelericon.eot");
+    src: url("./assets/font/travelericon.eot?#iefix") format("embedded-opentype"), url("./assets/font/travelericon.woff") format("woff"), url("./assets/font/travelericon.ttf") format("truetype"), url("./assets/font/travelericon.svg#travelericon") format("svg");
+    font-style: normal;
+    font-weight: normal;
+  }
+  
+  [class^="icon-"]:before,
+  [class*=" icon-"]:before {
+    font-family: "travelericon";
+    font-style: normal;
+    font-variant: normal;
+    text-transform: none;
+    speak: none;
+    line-height: 1;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  .icon-delete:before {
+    content: "\62";
+  }
+  
+  .icon-down:before {
+    content: "\63";
+  }
+  
+  .icon-hamburger:before {
+    content: "\64";
+  }
+  
+  .icon-next:before {
+    content: "\65";
+  }
+  
+  .icon-up:before {
+    content: "\66";
+  }
+  
+  .icon-back:before {
+    content: "\61";
+  }
+  
+  .icon-search:before {
+    content: "\67";
+  }
+  
+  #app {
+    font-family: 'Nanum Square', 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: left;
+    color: #0a0908;
+  }
+  
+  [v-cloak] {
+    display: none
+  }
+  
+  .a11y-hidden {
+    @extend %readable-hidden;
+  }
 </style>
